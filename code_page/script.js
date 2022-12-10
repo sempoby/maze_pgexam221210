9a857c587ed4f53a657e52d6b7724218fa59bbb6
sempoby <70sempoby@gmail.com>
2022-12-10 21:48:11 +0900

    no message

--- code_page/script.js
+++ code_page/script.js
@@ -52,7 +52,7 @@ function to2() {
 
 	function ans()
 	{
-		if(input.value == "파이썬" or input.value == "python") // [문1]의 정답부분
+		if(input.value == "파이썬")// [문1]의 정답부분
 			location.href="./page02_python.html";
 		else
 		{
